837593a3f806333fcd98372fb1b457d4a57daa5a
zhangjie <357688981@qq.com>
2018-01-11 22:35:10 +0800

    update

--- python/003-path_utils.py
+++ python/003-path_utils.py
@@ -3,7 +3,7 @@
 
 """
 * CREATED TIME: 2018-1-10 23:06:24
-* LAST MODIFIED TIME: 2018-1-10 23:06:24
+* LAST MODIFIED TIME: 2018-1-11 22:08:23
 * DESCRIPTION: Some utility methods for getting and manipulating paths
 * BY: 357688981@qq.com
 """
@@ -29,9 +29,61 @@ def MaybeMakeDirectory(*path):
         if e.errno != errno.EEXIST:
           raise
 
+
+def ListDirAndFiles(dir):
+    """Lists all the files and directory inside dir."""
+    return sorted(
+      x for x in os.listdir(dir)
+      if not x.startswith('.')
+    )
+
+
 if __name__ == '__main__':
     print("CurrentScriptDir: %s" % GetCurrentScriptDir())
 
     MaybeMakeDirectory("c:\\a\\b\\c\\d")
 
+    print(ListDirAndFiles('D:\\MyGithub\\useful-codes'))
+
     pass
+
+
+
+
+"""
+创建/删除临时文件夹
+"""
+import subprocess
+import sys
+import tempfile
+
+
+g_temp_dirs = []
+
+def TempDir():
+  """Generate a temporary directory (for downloading or extracting to) and keep
+  track of the directory that's created for cleaning up later."""
+  global g_temp_dirs
+  temp = tempfile.mkdtemp()
+  g_temp_dirs.append(temp)
+  return temp
+
+def RunOrDie(command):
+  rc = subprocess.call(command, shell=True)
+  if rc != 0:
+    raise SystemExit('%s failed.' % command)
+
+def DeleteAllTempDirs():
+  """Remove all temporary directories created by |TempDir()|."""
+  global g_temp_dirs
+  if g_temp_dirs:
+    sys.stdout.write('Cleaning up temporaries...\n')
+  for temp in g_temp_dirs:
+    # shutil.rmtree errors out on read only attributes.
+    RunOrDie('rmdir /s/q "%s"' % temp)
+  g_temp_dirs = []
+
+
+if __name__ == '__main__':
+    print(TempDir())
+    DeleteAllTempDirs()
--- python/README.txt
+++ python/README.txt
@@ -2,3 +2,4 @@
 002-set_environment_variable: 查看system platform, 设置注册表, 环境变量
 003-path_utils: Some utility methods for getting and manipulating paths
 004-process_utils: Shared process-related utility functions
+005-logging_utils: Utility functions and objects for logging
